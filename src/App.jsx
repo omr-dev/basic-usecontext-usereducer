03dab5bc267ac2ccee159e314f7eea69f0a35dce
omer <jagenau49@gmail.com>
2022-08-08 14:01:22 +0200

    useReducer in

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import './App.scss';
 import { AppContext } from './AppContext';
 
 function App() {
-	const {count, setCount} = useContext(AppContext);
-	function changeCounter(changeType) {
-		if (changeType === '-') {
-			setCount(count - 1);
-		} else if (changeType === '+') {
-			setCount(count + 1);
-		}
-	}
+	const { state, dispatch } = useContext(AppContext);
+	// function changeCounter(changeType) {
+	// 	if (changeType === '-') {
+	// 		setCount(count - 1);
+	// 	} else if (changeType === '+') {
+	// 		setCount(count + 1);
+	// 	}
+	// }
 	return (
 		<div className="App">
 			<h1>useContext/useReducer basic app</h1>
 			<h2>Count</h2>
 			<div className="CountBox">
-				<button onClick={() => changeCounter('-')}>-</button>
-				<button onClick={() => changeCounter('+')}>+</button>
-				<p>{count}</p>
+				<button onClick={() => dispatch('decreaseCount')}>-</button>
+				<button onClick={() => dispatch('increaseCount')}>+</button>
+				<p>{state.count}</p>
 			</div>
 		</div>
 	);
--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react';
 import { createContext } from 'react';
+import { useReducer } from 'react';
 
 export const AppContext = createContext();
+
+const initialState = {
+	count: 0,
+};
+
+function reducer(state, action) {
+	let obj = { ...state };
+	switch (action) {
+		case 'increaseCount':
+			obj.count++;
+			break;
+		case 'decreaseCount':
+			obj.count--;
+			break;
+	}
+	return obj;
+}
+
 export const AppProvider = ({ children }) => {
-	const [count, setCount] = useState(0);
+	const [state, dispatch] = useReducer(reducer, initialState);
 	return (
 		<AppContext.Provider
 			value={{
-				count,
-				setCount,
+				state,
+				dispatch,
 			}}
 		>
 			{children}
